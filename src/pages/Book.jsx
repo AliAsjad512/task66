29b1287eafab5311bf71eea16f8cca7ae9506ea5
AliAsjad512 <aa0504503@gmail.com>
2023-03-18 22:07:36 -0400

    task6

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,66 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-// import ErrorAlert from '../components/ErrorAlert';
-// import Skeleton from 'react-loading-skeleton'
-// import 'react-loading-skeleton/dist/skeleton.css'
-// import Container from '../components/Container';
-
-
-// const Book = () => {
-//   const [book, setBook] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(false);
-
-//   const { id } = useParams();
-
-//   const getData = async () => {
-//     const url = `https://api.matgargano.com/api/books/${id}`;
-//     setLoading(true);
-//     setError(false);
-
-//     try {
-//       const response = await fetch(url, { mode: 'cors' });
-//       const data = await response.json();
-//       setBook(data);
-//     } catch (e) {
-//       setError('Error: ' + e.message);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     getData();
-//   }, []);
-
-//   return (
-//     <Container>
-
-    
-
-//       {error && <ErrorAlert>{error}</ErrorAlert>}
-//       {loading && <Skeleton count={3} />}
-//       <>
-//       {book && (
-//         <div>
-//           <h2 >TITLE: {book.title}</h2>
-//           <p >Author: {book.author}</p>
-//           <p >Publisher: {book.publisher}</p>
-//           <p>Publication Year: {book.year}</p>
-//           <p >Number of Pages: {book.pages}</p>
-//           <p >Country Origin: {book.country}</p>
-//           <img  src={book.imageURL} alt="" />
-
-//         </div>
-//       )}
-//       </>
-        
-//     </Container>
-
-//   );
-// };
-
-// export default Book;
 
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
@@ -68,6 +5,7 @@ import ErrorAlert from '../components/ErrorAlert';
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import Container from '../components/Container';
+import './Book.css'
 
 const Book = () => {
 
@@ -105,8 +43,13 @@ const Book = () => {
             <>
             {book && (
              <div>
-                <p> TITLE: {book.title}</p>
-                <p> AUTHOR: {book.author}</p>
+                <p className='title'> TITLE: {book.title}</p>
+                <p className='author'> AUTHOR: {book.author}</p>
+                <p className='Publisher' >Publisher: {book.publisher}</p>
+          <p className='year'>Publication Year: {book.year}</p>
+          <p className='pages'>Number of Pages: {book.pages}</p>
+          <p className='orgin'>Country Origin: {book.country}</p>
+          <img  src={book.imageURL} alt="" />
                   
                 </div>
             )} 
